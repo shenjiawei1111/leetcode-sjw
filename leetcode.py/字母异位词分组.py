cf20cfd973f4b1b309d9fa1c44fcd5f1a970184a
沈家伟 <15105566366@163.com>
2025-08-24 16:57:11 +0800

    refactor: 将函数重构为类方法并简化字母异位词分组逻辑
    使用sorted替代计数排序来简化实现，同时将独立函数重构为Solution类的方法

--- leetcode.py/字母异位词分组.py
+++ leetcode.py/字母异位词分组.py
@@ -1,19 +1,13 @@
 from collections import defaultdict
 
-# 优化版本：使用计数排序代替普通排序，提高效率
-# 时间复杂度：O(n * k)，其中n是字符串数量，k是字符串的最大长度
-def groupAnagrams(strs):
-    mp = defaultdict(list)
-    for st in strs:
-        # 创建一个长度为26的计数数组，统计每个字符出现的次数
-        count = [0] * 26
-        for char in st:
-            count[ord(char) - ord('a')] += 1
-        # 将计数数组转换为元组作为字典的键
-        key = tuple(count)
-        mp[key].append(st)
-    return list(mp.values())
-
-# 测试代码
-strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
-print(groupAnagrams(strs))
+class Solution(object):
+    def groupAnagrams(self,strs):
+        mp = defaultdict(list)
+        for st in strs:
+            key = ''.join(sorted(st))
+            mp[key].append(st)
+        return list(mp.values())
+        
+c = Solution()
+strs = ["eat","tea","tan","ate","nat","bat"]
+print(c.groupAnagrams(strs))
